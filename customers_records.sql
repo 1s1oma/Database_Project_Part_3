b8659966a6fd8b4abe3392e8e5aa37d715ab9aa4
sarvarghese <sarvarghese@users.noreply.github.com>
2017-11-16 17:36:03 -0600

    Add files via upload
    20 customers information

---
+++ customers_records.sql
@@ -0,0 +1,24 @@
+
+
+
+INSERT INTO `person`(`ID_No`, `Name`, `Phone_no`, `Address`, `Email`, `Username`, `Password`, `Created_date`) VALUES 
+(1, "Noah", 1234567890, "1 Sesame Street", "noah@aol.com", "noah1", "password", 2017-01-01),
+(2, "Violet", 9087654321, "1 Hacker Way", "violet@yahoo.com", "violet2", "password", 2017-01-01),
+(3, "Billy", 1029384756, "1600 Pennsylvania Ave", "billy@gmail.com", "billy3", "password", 2017-01-01),
+(4, "Nicki", 5647382930, "2 Barbie Road", "nicki@outlook.com", "nicki4", "password", 2017-01-01),
+(5, "Cortez", 6789998212, "1 Superman Way", "souljaboy@hotmail.com", "cortez5", "password", 2017-01-01),
+(6, "Reginae", 2025550116, "2 Goldfied Road", "Reginae@aol.com", "reginae6", "password", 2017-01-01),
+(7, "Dwayne", 5185550192, "8 Pilgrim Street", "dwayne@aol.com", "dwayne7", "password", 2017-01-01),
+(8, "Cristal", 4105550135, "3 Magolia Road", "cristal@aol.com", "cristal8", "password", 2017-01-01),
+(9, "Katie", 4045550167, "19 Armstrong Road", "katie@aol.com", "katie9", "password", 2017-01-01),
+(10, "Mckenna", 2075550176, "93 Harbour Lane", "mckenna@aol.com", "mckenna10", "password", 2017-01-01),
+(11, "Alisa", 5125550176, "28 Rochester Court", "alisa@aol.com", "alisa11", "password", 2017-01-01),
+(12, "Daniel", 2255550123, "38 Arlington Way", "daniel@aol.com", "daniel12", "password", 2017-01-01),
+(13, "Ben", 7015550183, "8 County Street", "ben@aol.com", "ben13", "password", 2017-01-01),
+(14, "Joey", 2085550129, "9 Lantern Drive", "joey@aol.com", "joey14", "password", 2017-01-01),
+(15, "Paul", 6172220192, "2 Spruce Road", "paul@aol.com", "paul15", "password", 2017-01-01),
+(16, "Zayn", 3025550198, "9 Lake Ave", "zayn@aol.com", "zayn16", "password", 2017-01-01),
+(17, "Aubrey", 6148290192, "294 Market Street", "aubrey@aol.com", "aubrey17", "password", 2017-01-01),
+(18, "Sean", 5059231829, "9 Main Street", "sean@aol.com", "sean18", "password", 2017-01-01),
+(19, "Isioma", 8080123838, "1 Apple Way", "isioma@aol.com", "isioma19", "password", 2017-01-01),
+(20, "Sarah",  4356719821, "4 Count Lane", "sarah@aol.com", "sarah20","password", 2017-01-01);
